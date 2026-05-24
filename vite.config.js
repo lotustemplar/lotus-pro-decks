e08c383c4c6b78934fd843e6fbf974ce706c38dc
lotustemplar <filipetheology@gmail.com>
2026-05-24 03:03:31 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/propilot-decks/',
+  base: '/',
   server: {
     port: parseInt(process.env.PORT || '5173'),
     strictPort: false,
